11440b9027a151aac626fe82ea63759b5414fe98
Kaiden <kaidenrivett@uvic.ca>
2020-09-27 23:22:40 -0700

    Committing PMan with progress on bg_entry with fork and execvp

--- PMan.c
+++ PMan.c
@@ -50,8 +50,8 @@ struct node *current = NULL;
  */
 
 // Funtion Prototypes
-// int bg_entry(char**);
-// void bg_list(int);
+void bg_entry(char** argv);
+// void bg_list();
 // void status_change(pid_t, int);
 // void pstat_entry(pid_t);
 // void bg_process_update();
@@ -95,43 +95,37 @@ int main() {
 	  cmd[i] = strtok(input, " ");
 		while(cmd[i]!=NULL){
 			cmd[++i] = strtok(NULL, " ");
+			// printf("%s\n",cmd[i]);
 		}
-		printf("%s\n",cmd[0]);
 
-if(strcmp(cmd[1], "bglist") == 0){
-	printf("Yay it works!\n");
+
+if(strcmp(cmd[0], "bg") == 0){
+	bg_entry(cmd);
 } else {
-	for(int i = 0; i <strlen(*cmd); i++){
-	printf("Oh no, you printed %s", cmd[i]);
-}
+	perror("User input needed");
 }
 return 0;
 }
 
 
-
-
-
-
-
-	FILE *fp;
-	char line[128];
-	NODE *current, *head;
-
-	fp = fopen("Lipsum.txt", "r");
-
-	while(fgets(line, sizeof(line), fp)){
-		NODE *node = malloc(sizeof(NODE));
-		node->data = strdup(line);
-		node->next = NULL;
-
-		if(head == NULL){
-			current = head = node;
-		} else {
-			current = current->next = node;
-		}
-	}
-	fclose(fp);
+	// FILE *fp;
+	// char line[128];
+	// NODE *current, *head;
+	//
+	// fp = fopen("Lipsum.txt", "r");
+	//
+	// while(fgets(line, sizeof(line), fp)){
+	// 	NODE *node = malloc(sizeof(NODE));
+	// 	node->data = strdup(line);
+	// 	node->next = NULL;
+	//
+	// 	if(head == NULL){
+	// 		current = head = node;
+	// 	} else {
+	// 		current = current->next = node;
+	// 	}
+	// }
+	// fclose(fp);
 
 	// print the linked list
 
@@ -141,7 +135,31 @@ return 0;
 	return 0;*/
 }
 
-void bg_list() {
-	printf("You are in the bglist!");
-
+void bg_entry(char **argv) {
+	char *cmd_in[MAX_INPUT_SIZE];
+// 	size_t n = sizeof(argv);
+// 	printf("Size of the argument array is: %d\n", n);
+// 	if(sizeof(argv) == 4){
+// 		printf("n equals 3!\n");
+// 	cmd_in[0] = argv[1];
+// 	cmd_in[1] = argv[2];
+// 	cmd_in[2] = argv[3];
+// } else {
+// 	printf("n equals 2\n");
+// 	cmd_in[0] = argv[1];
+// 	cmd_in[1] = argv[2];
+// }
+	pid_t pid;
+	pid = fork();
+	if(pid == 0){
+		if(execvp(argv[1], argv) < 0){
+			perror("Error on execvp");
+		}
+		exit(EXIT_SUCCESS);
+	} else if(pid > 0){
+		// store information of the background child process in data structures here
+	} else {
+		perror("fork failed");
+		exit(EXIT_FAILURE);
+	}
 }
